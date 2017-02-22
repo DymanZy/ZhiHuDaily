f6e97f6069a4dcbd166d2738a90def047eec58c8
Dyman <dyman_zy@163.com>
2017-02-22 17:02:57 +0800

    文章页面添加 Toolbar 滑动透明和 HeaderImage 的滑动移出

--- app/src/main/java/com/dyman/zhihudaily/module/news/NewsDetailActivity.java
+++ app/src/main/java/com/dyman/zhihudaily/module/news/NewsDetailActivity.java
@@ -1,6 +1,6 @@
 package com.dyman.zhihudaily.module.news;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
@@ -9,31 +9,39 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ScrollView;
 
 import com.dyman.zhihudaily.R;
 import com.dyman.zhihudaily.base.IntentKeys;
 import com.dyman.zhihudaily.entity.NewsDetailInfo;
 import com.dyman.zhihudaily.network.RetrofitHelper;
-import com.dyman.zhihudaily.network.api.ZhiHuAppService;
+import com.dyman.zhihudaily.utils.ScrollPullDownHelper;
 import com.dyman.zhihudaily.utils.ToastUtil;
+import com.dyman.zhihudaily.widget.MyImageTextLayout;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class NewsDetailActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+public class NewsDetailActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, ViewTreeObserver.OnScrollChangedListener{
 
     private static final String TAG = NewsDetailActivity.class.getSimpleName();
 
     private int newsID;
 
-    private SwipeRefreshLayout mSwipeRefresh;
+    private MyImageTextLayout imageTextLayout;
+
+    private ScrollView mScrollView;
 
     private WebView webView;
+
+    private Toolbar toolbar;
+
+    private ScrollPullDownHelper mScrollPullDownHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +50,13 @@ public class NewsDetailActivity extends AppCompatActivity implements SwipeRefres
 
         initToolbar();
         init();
+
+        mScrollPullDownHelper = new ScrollPullDownHelper();
     }
 
 
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("文章详情");
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -60,17 +70,19 @@ public class NewsDetailActivity extends AppCompatActivity implements SwipeRefres
 
         // 初始化数据
         newsID = getIntent().getIntExtra(IntentKeys.NEWS_ID, 0);
-        //  初始化下拉刷新控件
-        mSwipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh_activity_news_detail);
-        mSwipeRefresh.setColorSchemeResources(android.R.color.holo_blue_light, R.color.colorAccent,
-                android.R.color.holo_red_light);
-        mSwipeRefresh.setOnRefreshListener(this);
+        //  头部控件
+        imageTextLayout = (MyImageTextLayout) findViewById(R.id.container_header_activity_news_detail);
+        //  初始化 ScrollView 及其滑动监听
+        mScrollView = (ScrollView) findViewById(R.id.scrollView_activity_news_detail);
+        mScrollView.setOverScrollMode(View.OVER_SCROLL_NEVER); //去掉滑动到底部的蓝色阴影
+        mScrollView.getViewTreeObserver().addOnScrollChangedListener(this);
         //  初始化网页显示控件
         webView = (WebView) findViewById(R.id.webView_activity_news_detail);
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         webView.getSettings().setJavaScriptEnabled(true);// 设置支持JavaScript
+        webView.loadUrl("https://www.baidu.com/");
 
-        loadData(String.valueOf(newsID));
+//        loadData(String.valueOf(newsID));
     }
 
 
@@ -87,14 +99,12 @@ public class NewsDetailActivity extends AppCompatActivity implements SwipeRefres
 
                     @Override
                     public void onError(Throwable e) {
-                        mSwipeRefresh.setRefreshing(false);
                         ToastUtil.ShortToast("加载数据失败");
                         e.printStackTrace();
                     }
 
                     @Override
                     public void onNext(NewsDetailInfo newsDetailInfo) {
-                        mSwipeRefresh.setRefreshing(false);
                         // TODO: update UI
                         showHtml(newsDetailInfo.getBody());
                     }
@@ -110,7 +120,7 @@ public class NewsDetailActivity extends AppCompatActivity implements SwipeRefres
 
     @Override
     public void onRefresh() {
-        loadData(String.valueOf(newsID));
+//        loadData(String.valueOf(newsID));
     }
 
 
@@ -126,4 +136,36 @@ public class NewsDetailActivity extends AppCompatActivity implements SwipeRefres
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onScrollChanged() {
+
+        //  改变 HeaderView 的位置
+        int scrollY = mScrollView.getScrollY();
+        int headerScrollY = (scrollY > 0) ? (scrollY / 2) : 0;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            imageTextLayout.setScrollY(headerScrollY);
+            imageTextLayout.requestLayout();//view 的位置改变,请求重新绘制
+        }
+
+        //TODO: 改变 Toolbar 的透明度
+        int storyHeaderViewHeight = getResources().getDimensionPixelSize(R.dimen.view_header_story_height);
+        float toolbarHeight = toolbar.getHeight();
+        float contentHeight = storyHeaderViewHeight - toolbarHeight;
+
+        float ratio = Math.min(scrollY / contentHeight, 1.0f);
+        toolbar.setAlpha(1-ratio);
+        Log.i(TAG, "onScrollChanged: ratio="+ratio);
+        if (scrollY <= contentHeight) {
+            toolbar.setY(0f);
+            return;
+        }
+
+        boolean isPullingDown = mScrollPullDownHelper.onScrollChange(scrollY);
+        float toolBarPositionY = isPullingDown ? 0 : (contentHeight - scrollY);
+        toolbar.setY(toolBarPositionY);
+        toolbar.setAlpha(1f);
+
+
+
+    }
 }
---
+++ app/src/main/java/com/dyman/zhihudaily/utils/ScrollPullDownHelper.java
@@ -0,0 +1,44 @@
+package com.dyman.zhihudaily.utils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by dyman on 2017/2/22.
+ */
+
+public class ScrollPullDownHelper {
+
+    private static final int PULLING_DOWN_TIME_MAX = 8;
+    private static final int PULLING_DOWN_TIME_THRESHOLD = 6;
+
+    private int lastScrollY;
+    private final Queue<Boolean> lastestPullingDown;
+
+    public ScrollPullDownHelper() {
+        lastScrollY = 0;
+        lastestPullingDown = new LinkedList<>();
+    }
+
+    //记录最近8次的滚动,如果最近的8次当中有6次是向下滚动,则判断为向下滚动
+    public boolean onScrollChange(int scrollY) {
+        boolean isPullingDownNow = scrollY < lastScrollY;
+        lastestPullingDown.offer(isPullingDownNow);
+        if (lastestPullingDown.size() > PULLING_DOWN_TIME_MAX) {
+            lastestPullingDown.poll();
+        }
+        lastScrollY = scrollY;
+
+        return getPullingDownTime() >= PULLING_DOWN_TIME_THRESHOLD;
+    }
+
+    private int getPullingDownTime() {
+        int result = 0;
+        for (Boolean isPullingDown : lastestPullingDown) {
+            if (isPullingDown) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
