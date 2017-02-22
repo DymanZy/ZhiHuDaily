b2f08c1c313d63b52fbb2dffc1a8e6e5cf3ffaf5
Dyman <dyman_zy@163.com>
2017-02-23 00:36:14 +0800

    修改评论界面

--- app/src/main/java/com/dyman/zhihudaily/adapter/CommentAdapter.java
+++ app/src/main/java/com/dyman/zhihudaily/adapter/CommentAdapter.java
@@ -8,9 +8,13 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.dyman.zhihudaily.R;
+import com.dyman.zhihudaily.adapter.listener.AdapterItemClickListener;
 import com.dyman.zhihudaily.entity.CommentsInfo;
+import com.dyman.zhihudaily.utils.TimeUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -22,10 +26,16 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHolder> {
 
     private Context context;
-    private List<CommentsInfo> datas;
+    private List<CommentsInfo.CommentsBean> datas;
+
+    private AdapterItemClickListener listener;
+    public void setAdapterItemClickListener(AdapterItemClickListener listener) {
+        this.listener = listener;
+    }
 
     public CommentAdapter(Context context) {
         this.context = context;
+        datas = new ArrayList<>();
     }
 
 
@@ -38,6 +48,14 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHo
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         //TODO: put data to widget
+        CommentsInfo.CommentsBean commentsBean = datas.get(position);
+
+        Glide.with(context).load(commentsBean.getAvatar()).into(holder.circleImageView);
+        holder.userNameTv.setText(commentsBean.getAuthor());
+        holder.markNumsTv.setText(commentsBean.getLikes());
+        holder.contentTv.setText(commentsBean.getContent());
+        holder.timeTv.setText(TimeUtil.parseDateTime(commentsBean.getTime()));
+
     }
 
     @Override
@@ -45,13 +63,28 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHo
         return datas.size();
     }
 
+
+    /**
+     *  更新 adapter 中的数据
+     * @param beanList
+     */
+    public void updateAdapter(List<CommentsInfo.CommentsBean> beanList) {
+
+        datas.clear();
+        for (int i = 0, len = beanList.size(); i < len; i++) {
+            datas.add(beanList.get(i));
+        }
+        notifyDataSetChanged();
+    }
+
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         private LinearLayout bodyLl;
         private CircleImageView circleImageView;
         private TextView userNameTv;
         private TextView markNumsTv;
-        private TextView commentInfoTv;
+        private TextView contentTv;
         private TextView timeTv;
 
         public MyViewHolder(View itemView) {
@@ -61,8 +94,17 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.MyViewHo
             circleImageView = (CircleImageView) itemView.findViewById(R.id.userImage_civ_item_comment);
             userNameTv = (TextView) itemView.findViewById(R.id.userName_tv_item_comment);
             markNumsTv = (TextView) itemView.findViewById(R.id.markNum_tv_item_comment);
-            commentInfoTv = (TextView) itemView.findViewById(R.id.commentInfo_tv_item_comment);
+            contentTv = (TextView) itemView.findViewById(R.id.content_tv_item_comment);
             timeTv = (TextView) itemView.findViewById(R.id.time_tv_item_comment);
+
+            bodyLl.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onAdapterItemClick(getLayoutPosition());
+                    }
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/dyman/zhihudaily/module/news/CommentActivity.java
+++ app/src/main/java/com/dyman/zhihudaily/module/news/CommentActivity.java
@@ -6,11 +6,22 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.dyman.zhihudaily.R;
 import com.dyman.zhihudaily.ZhiHuDailyApp;
+import com.dyman.zhihudaily.adapter.CommentAdapter;
+import com.dyman.zhihudaily.adapter.listener.AdapterItemClickListener;
 import com.dyman.zhihudaily.base.BaseActivity;
+import com.dyman.zhihudaily.base.IntentKeys;
+import com.dyman.zhihudaily.entity.CommentsInfo;
+import com.dyman.zhihudaily.network.RetrofitHelper;
 import com.dyman.zhihudaily.utils.DividerItemDecoration;
+import com.dyman.zhihudaily.utils.ToastUtil;
+
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 public class CommentActivity extends BaseActivity {
@@ -23,6 +34,11 @@ public class CommentActivity extends BaseActivity {
 
     private RecyclerView shortCommentRv;
 
+    private CommentAdapter longCAdapter;
+    private CommentAdapter shortCAdapter;
+
+    private int newID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +47,14 @@ public class CommentActivity extends BaseActivity {
 
         initToolbar();
         initView();
+        initData();
 
-
+        loadData();
     }
 
+
     private void initToolbar() {
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("xx条点评");
         setSupportActionBar(toolbar);
@@ -47,17 +66,88 @@ public class CommentActivity extends BaseActivity {
 
 
     private void initView() {
+
+        longCAdapter = new CommentAdapter(ZhiHuDailyApp.getInstance());
+        shortCAdapter = new CommentAdapter(ZhiHuDailyApp.getInstance());
+
         longCommentRv = (RecyclerView) findViewById(R.id.longComment_rv_activity_comment);
         longCommentRv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         longCommentRv.setItemAnimator(new DefaultItemAnimator());
         longCommentRv.addItemDecoration(new DividerItemDecoration(ZhiHuDailyApp.getInstance(),
                 DividerItemDecoration.HORIZONTAL_LIST));
+        longCommentRv.setAdapter(longCAdapter);
+        longCAdapter.setAdapterItemClickListener(new AdapterItemClickListener() {
+            @Override
+            public void onAdapterItemClick(int position) {
+                ToastUtil.ShortToast("点击了长评论 第"+position+"个");
+            }
+        });
 
         shortCommentRv = (RecyclerView) findViewById(R.id.shortComment_rv_activity_comment);
-        longCommentRv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        longCommentRv.setItemAnimator(new DefaultItemAnimator());
-        longCommentRv.addItemDecoration(new DividerItemDecoration(ZhiHuDailyApp.getInstance(),
+        shortCommentRv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        shortCommentRv.setItemAnimator(new DefaultItemAnimator());
+        shortCommentRv.addItemDecoration(new DividerItemDecoration(ZhiHuDailyApp.getInstance(),
                 DividerItemDecoration.HORIZONTAL_LIST));
+        shortCommentRv.setAdapter(shortCAdapter);
+        shortCAdapter.setAdapterItemClickListener(new AdapterItemClickListener() {
+            @Override
+            public void onAdapterItemClick(int position) {
+                ToastUtil.ShortToast("点击了长评论 第"+position+"个");
+            }
+        });
     }
 
+
+    private void initData() {
+
+        newID = getIntent().getIntExtra(IntentKeys.NEWS_ID, 0);
+    }
+
+
+    private void loadData() {
+
+        RetrofitHelper.getZhiHuAPI()
+                .getCommentsLong(String.valueOf(newID))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CommentsInfo>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i(TAG, "--load long comment complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        //TODO: set EmptyView
+                    }
+
+                    @Override
+                    public void onNext(CommentsInfo commentsInfo) {
+                        longCAdapter.updateAdapter(commentsInfo.getComments());
+                    }
+                });
+
+        RetrofitHelper.getZhiHuAPI()
+                .getCommentsShort(String.valueOf(newID))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CommentsInfo>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i(TAG, "--load short comment complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        //TODO: set EmptyView
+                    }
+
+                    @Override
+                    public void onNext(CommentsInfo commentsInfo) {
+                        shortCAdapter.updateAdapter(commentsInfo.getComments());
+                    }
+                });
+    }
 }
